c916742ec14a7d097395ae6f06c5581b24e33703
BrunoGomesCoelho <bruno.gomes.coelho@usp.br>
2018-05-26 19:06:09 -0300

    Remove working example

--- visulization.py
+++ visulization.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets
 
 from OpenGL import GL as gl
 from OpenGL import GLU as glu
-# from OpenGL import GLUT
+from OpenGL import GLUT as glut
 
 class VisualizationWidget(QtWidgets.QOpenGLWidget):
 
@@ -33,13 +33,16 @@ class VisualizationWidget(QtWidgets.QOpenGLWidget):
 
     # Test fucntion for now
     def paintGL(self):
+        """
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
         gl.glColor3f(1, 0, 0)
         gl.glBegin(gl.GL_TRIANGLES)
         gl.glVertex3f(-0.5, -0.5, 0)
         gl.glVertex3f(0.5, -0.5, 0)
         gl.glVertex3f(0.0, 0.5, 0)
+        glut.glutWireCube(1)
         gl.glEnd()
 
         glu.gluPerspective(45, 651/551, 0.1, 50.0)
         gl.glTranslatef(0.0, 0.0, -5)
+        """
